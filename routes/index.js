8912aef76d63ef7b61afce49ce22b5313cbcd3af
moJiXiang <mojixiang1102@gmail.com>
2015-10-21 21:45:00 +0800

    hanzhen project

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.locals.label = 'index';
+  res.render('index');
+});
+
+router.get('/product', function(req, res) {
+    res.locals.label = 'product';
+  res.render('product');
+});
+
+router.get('/agentsearch', function(req, res) {
+    res.locals.label = 'agentsearch';
+  res.render('agentsearch');
+});
+
+router.get('/instruction', function(req, res) {
+    res.locals.label = 'instruction';
+  res.render('instruction');
+});
+
+router.get('/certification', function(req, res) {
+    res.locals.label = 'certification';
+  res.render('certification');
+});
+
+router.get('/qrcode', function(req, res) {
+  res.render('qrcode');
+});
+
+router.get('/telephone', function(req, res) {
+  res.render('telephone');
+});
+
+module.exports = router;
