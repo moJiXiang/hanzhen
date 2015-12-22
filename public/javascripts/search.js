af6d999b1e4ad176ffb03282415f6c2d5aca129a
moJiXiang <mojixiang1102@gmail.com>
2015-12-22 16:20:58 +0800

    add

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -50,7 +50,9 @@ $(document).ready(function() {
             {"name": "738168104", "where": "分销"},
             {"name": "h2695525776", "where": "一级代理"},
             {"name": "a83482000", "where": "一级代理"},
-            {"name": "13006549271", "where": "福建总代理"}
+            {"name": "13006549271", "where": "福建总代理"},
+            {"name": "jianghuld111", "where": "分销"},
+            {"name": "wbt-12", "where": "分销"}
         ];
 
         var isTrueShop, valiableshop;
